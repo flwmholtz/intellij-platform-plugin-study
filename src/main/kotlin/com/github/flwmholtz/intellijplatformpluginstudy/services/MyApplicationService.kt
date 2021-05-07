7fa89ad9ac163eeaa1f0922115878ffdc0ee20f8
GitHub Action <action@github.com>
2021-05-07 08:09:59 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/flwmholtz/intellijplatformpluginstudy/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.flwmholtz.intellijplatformpluginstudy.services
+
+import com.github.flwmholtz.intellijplatformpluginstudy.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
